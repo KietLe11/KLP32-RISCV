3e00d667322aa59b3843d3985765ef5d69460e37
Kiet Le <ktlbusiness11@gmail.com>
2024-11-10 23:27:37 -0500

    Allow 32 bit input to SRA

--- r-format-modules/sra32.v
+++ r-format-modules/sra32.v
@@ -3,7 +3,7 @@ module sra32(X, shift, result);
 
     parameter n = 32;
     input [n-1:0] X;
-    input [4:0] shift;
+    input [n-1:0] shift;
     output [n-1:0] result;
 
     assign result = (X >>> shift) | (32'h80000000 & X);
