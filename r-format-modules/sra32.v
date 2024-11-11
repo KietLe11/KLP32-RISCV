5b63a3dea59dbf9d331ac1ef153bf179bddbfccf
Kiet Le <ktlbusiness11@gmail.com>
2024-11-10 23:29:31 -0500

    small fix

--- r-format-modules/sra32.v
+++ r-format-modules/sra32.v
@@ -2,8 +2,7 @@
 module sra32(X, shift, result);
 
     parameter n = 32;
-    input [n-1:0] X;
-    input [n-1:0] shift;
+    input [n-1:0] X, shift;
     output [n-1:0] result;
 
     assign result = (X >>> shift) | (32'h80000000 & X);
