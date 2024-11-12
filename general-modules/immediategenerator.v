004a029c780d08d7d995d4319c0d5a3a5fca6887
Lawrence P <63280907+LLP17@users.noreply.github.com>
2024-11-12 13:14:55 -0500

    Fix syntax errors

--- general-modules/immediategenerator.v
+++ general-modules/immediategenerator.v
@@ -10,9 +10,9 @@ module immmediategenerator(immediate,imm_sel, imm_extend);
         case(imm_sel)
             2'b000: imm_extend = {21{instr[31]}, instr[30:20]}; // I-Type Immediate extension 
             2'b001: imm_extend = {21{instr[31]}, instr[30:25], instr[11:7]}; // S-Type Immediate extension
-            2'b010: imm_extend = {20{instr[31]}, instr[7], instr[30:25], instr[4:1],1'b0} // B-Type Immediate extension
-            2'b011: imm_extend = {{instr[31]}, instr[30:20],instr[19:12], 12b'0} // U-Type Immediate extension
-            2'b100: imm_extend = {12{instr[31]}, instr[19:12],instr[20], instr[30:25], instr[24:21],1'b0} // J-Type Immediate extension 
+            2'b010: imm_extend = {20{instr[31]}, instr[7], instr[30:25], instr[4:1],1'b0}; // B-Type Immediate extension
+            2'b011: imm_extend = {{instr[31]}, instr[30:20],instr[19:12], 12b'0}; // U-Type Immediate extension
+            2'b100: imm_extend = {12{instr[31]}, instr[19:12],instr[20], instr[30:25], instr[24:21],1'b0}; // J-Type Immediate extension 
             defautl: imm_extend = 32'bx // Output an undefined result if the imm_sel is not valid
         endcase
 endmodule
