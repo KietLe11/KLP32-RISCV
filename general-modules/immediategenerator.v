58fd107d192d3364b9c8df2dadabca33834a06ea
Lawrence P <63280907+LLP17@users.noreply.github.com>
2024-11-12 13:28:20 -0500

    Fix spacing

--- general-modules/immediategenerator.v
+++ general-modules/immediategenerator.v
@@ -9,21 +9,21 @@ module immmediategenerator(instr,imm_sel, imm_extend);
     begin
         case(imm_sel)
             // I-Type Immediate extension 
-            2'b000: imm_extend = {21{instr[31]}, instr[30:20]};  
+            2'b000 : imm_extend = {21{instr[31]}, instr[30:20]};  
 
             // S-Type Immediate extension
-            2'b001: imm_extend = {21{instr[31]}, instr[30:25], instr[11:7]}; 
+            2'b001 : imm_extend = {21{instr[31]}, instr[30:25], instr[11:7]}; 
 
             // B-Type Immediate extension
-            2'b010: imm_extend = {20{instr[31]}, instr[7], instr[30:25], instr[4:1],1'b0}; 
+            2'b010 : imm_extend = {20{instr[31]}, instr[7], instr[30:25], instr[4:1],1'b0}; 
 
             // U-Type Immediate extension
-            2'b011: imm_extend = {{instr[31]}, instr[30:20],instr[19:12], 12b'0}; 
+            2'b011 : imm_extend = {{instr[31]}, instr[30:20],instr[19:12], 12b'0}; 
 
             // J-Type Immediate extension 
-            2'b100: imm_extend = {12{instr[31]}, instr[19:12],instr[20], instr[30:25], instr[24:21],1'b0};
-            
+            2'b100 : imm_extend = {12{instr[31]}, instr[19:12],instr[20], instr[30:25], instr[24:21],1'b0};
+
             // Output an undefined result if the imm_sel is not valid
-            defautl: imm_extend = 32'bx;
+            default : imm_extend = 32'bx;
         endcase
 endmodule
