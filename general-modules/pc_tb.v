3426cbbd72e29a02eee6399653459b65674eef25
Kiet Le <ktlbusiness11@gmail.com>
2024-11-22 16:48:20 -0500

    Implement a bunch of Muxes and small mem fixes
    - Fix instruction memory addressing to support multiples of 4 addressing
    - Implment PC, PC Incrementor, PC select Muxes
    - Implement alu input muxes for A and B, test benches to be implemented

---
+++ general-modules/pc_tb.v
@@ -0,0 +1,55 @@
+`timescale 1ns/1ps
+module pc__tb();
+
+    parameter n = 32;
+    reg [n-1:0] pc;
+    wire [n-1:0] result;
+    reg clk;
+    reg [n-1:0] expected_result;
+
+    pc program_counterl(.clk(clk), .reset(1'b0), .pc_in(pc), .pc_out(result));
+
+    always begin
+        clk = 1'b1;
+        #10;
+        clk = 1'b0;
+        #10;
+    end
+
+    initial begin
+        // Initialize values
+        pc = 0;
+
+        #20;
+
+        // Test Case 1
+        pc = 32'h00510193;
+        expected_result = pc;
+        #20
+        check_result("Test Case 1");
+
+        // Test Case 2
+        pc = 32'h00100093;
+        expected_result = pc;
+        #20
+        check_result("Test Case 2");
+
+        // Test Case 3: Change pc_in mid clock
+        pc = 32'h00100093;
+        #5
+        pc = 32'h76767676;
+        expected_result = 32'h00100093; // Result should still be same as TC2
+        #15
+        check_result("Test Case 3");
+
+    end
+
+    task check_result(input [255:0] test_name);
+        if (result == expected_result) begin
+            $display("PASS: %s. Expected: %d, Got: %d", test_name, expected_result, result);
+        end else begin
+            $display("FAIL: %s. Expected: %d, Got: %d", test_name, expected_result, result);
+        end
+    endtask
+
+endmodule
---
+++ misc-modules/pc_increment_tb.v
@@ -0,0 +1,45 @@
+`timescale 1ns/1ps
+module pc_increment_tb();
+
+    parameter n = 32;
+    reg [n-1:0] pc;
+    wire [n-1:0] result;
+    reg clk;
+    reg [n-1:0] expected_result;
+
+    pc_increment pcPlus4(.pc_in(pc), .pc_out(result));
+
+    always begin
+        clk = 1'b1;
+        #10;
+        clk = 1'b0;
+        #10;
+    end
+
+    initial begin
+        // Initialize values
+        pc = 0;
+        #20;
+
+        // Test Case 1
+        expected_result = pc + 4;
+        #20
+        check_result("Test Case 1");
+
+        // Test Case 2
+        pc = 32'h00107600;
+        expected_result = pc + 4;
+        #20
+        check_result("Test Case 2");
+
+    end
+
+    task check_result(input [255:0] test_name);
+        if (result == expected_result) begin
+            $display("PASS: %s. Expected: %d, Got: %d", test_name, expected_result, result);
+        end else begin
+            $display("FAIL: %s. Expected: %d, Got: %d", test_name, expected_result, result);
+        end
+    endtask
+
+endmodule
