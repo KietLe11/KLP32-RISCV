c95fe17bef0b21d40913af6896ed8dd446294c5a
Kiet Le <ktlbusiness11@gmail.com>
2025-01-03 13:42:14 -0500

    Testing Immediate Instructions (addi, slti) (#37)
    * Start testing Immediate Instructions (addi, slti)
    
    * Add Comments

--- rtl/alu-modules/adder32.v
+++ rtl/alu-modules/adder32.v
@@ -1,11 +1,11 @@
 module adder32 (X, Y, result, overflow);
 
     parameter integer n = 32;
-    input [n-1:0] X, Y;
-    output [n-1:0] result;
+    input signed [n-1:0] X, Y;
+    output signed [n-1:0] result;
     output overflow;
 
     assign result = X + Y;
-    assign overflow = (X + Y) > 32'hFFFFFFFF;
+    assign overflow = (X[n-1] == Y[n-1]) && (X[n-1] != result[n-1]);
 
 endmodule
