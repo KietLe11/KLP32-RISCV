c173fb2fba15fc0963f320da71eeb29a4df9dca9
S J <17.shoaibj@gmail.com>
2025-04-21 18:46:39 -0400

    Add main_v2.sv into rtl/

--- rtl/main_v2.sv
+++ rtl/main_v2.sv
@@ -1,15 +1,38 @@
-module main_v2 (clk, reset_in);
+module main_v2 
+    (
+        input logic clk, 
+        input logic reset_in
 
-    input clk, reset_in;
-    logic reset;
+    );
 
     // Button on DE10 lite is default high unpressed
     // Change according to needs
+    logic reset;
     assign reset = ~reset_in;
 
-    KLP32V2 processor(
-        .clk(second_clk),
-        .reset(reset)
+    // Instantiate MMCM Clock Generator
+
+
+    // Instantiate CPU
+    KLP32V2 processor
+    (
+        .clk(clk),
+        .reset(reset),
+
+        // MMIO Bus
+        .bus_addr(bus_addr),
+        .bus_wr_data(bus_wr_data),
+        .bus_rd_data(bus_rd_data),
+        .bus_cs(bus_cs),
+        .bus_wr(bus_wr),
+        .bus_rd(bus_rd)
     );
+
+    //Instantiate Peripheral Module
+    io_top io_unit
+    (
+        
+    );
+    
 
 endmodule
